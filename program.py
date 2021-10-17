d72cec779815cae96aff5a734cfd388331434b90
Shalina Rohdenburg <shalinarohdenburg@gmail.com>
2021-10-17 14:28:19 +0200

    feat: read data from sampledata

---
+++ program.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+
+def read_input(filename='parkplatz0.txt'):
+    """ Beispieldatei einlesen
+    Die Zeilen in Integer und List umwandeln und Zeilenumbrüche mit .strip() entfernen.
+    Default ist das Aufgabenbeispiel parkplatz0.txt.
+    """
+    file = Path('sampledata', filename)
+    with open(file, 'r') as file_in:
+        parked_cars = file_in.readline().split()
+        moving_cars_total = file_in.readline().strip()
+        moving_cars = [line.strip() for line in file_in.readlines()]
+
+    print(parked_cars)
+    print(moving_cars_total)
+    print(moving_cars)
+
+    return parked_cars, moving_cars
+
+if __name__ == '__main__':
+    parked_cars, moving_cars = read_input()
