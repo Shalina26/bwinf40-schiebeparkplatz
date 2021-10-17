e55531c570432092da2c0764db46dead78a98569
Shalina Rohdenburg <shalinarohdenburg@gmail.com>
2021-10-17 14:46:21 +0200

    feat: create parkinglot from sampledata

--- program.py
+++ program.py
@@ -18,5 +18,42 @@ def read_input(filename='parkplatz0.txt'):
 
     return parked_cars, moving_cars
 
+
+def make_parkinglot(parked_cars, moving_cars):
+    """
+    Erstellt aus parked_cars und alphabet die Liste parkinglot mit allen Autos, die ausgeparkt werden sollen.
+    Erstellt aus moving_cars ein dict(), dessen key der Buchstabe des Autos und value die Nummer des Parkplatzes ist.
+    Gibt den Autos auf dem Parkplatz einen Index und prüft, welche Autos ausparken können und welche nicht.
+    """
+
+    alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+                "U", "V", "W", "X", "Y", "Z"]
+    parkinglot = []
+    end = parked_cars[1]
+
+    for letter in alphabet:
+        parkinglot.append(letter)
+        if letter == end:
+            break
+
+    occupiedlot = {}
+
+    for element in moving_cars:
+        letter, number = element.split()
+        number = int(number)
+        occupiedlot[letter] = number
+        occupiedlot[letter.lower()] = number + 1
+
+    print(occupiedlot)
+
+    for space in parkinglot:
+        index = parkinglot.index(space)
+        if index in occupiedlot.values():
+            print(f"{parkinglot[index]}: besetzt")
+        else:
+            print(f"{parkinglot[index]}: ")
+
+
 if __name__ == '__main__':
     parked_cars, moving_cars = read_input()
+    make_parkinglot(parked_cars, moving_cars)
